80973f180e6d9d8339597f61bf93d7cc7b102006
Ritika <ritika@Ritika.local>
2016-03-29 20:23:38 +0530

    Uncommenting code and writing uiwatcher

--- src/com/hike/test/NewGroupChat.java
+++ src/com/hike/test/NewGroupChat.java
@@ -1,12 +1,14 @@
 package com.hike.test; 
 
 import android.os.RemoteException;
+import android.util.Log;
 
 import com.android.uiautomator.core.UiDevice;
 
 
 
 import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiWatcher;
 import com.android.uiautomator.testrunner.*;
 import com.android.uiautomator.core.UiObjectNotFoundException;
 import com.android.uiautomator.core.UiSelector;
@@ -16,7 +18,7 @@ public class NewGroupChat extends UiAutomatorTestCase
 
 {
 
-	public void test001() throws UiObjectNotFoundException
+	public void test001_AppClick() throws UiObjectNotFoundException
 	
 	{ 
 		
@@ -30,7 +32,7 @@ public class NewGroupChat extends UiAutomatorTestCase
 	
 
 
-public void test002() throws UiObjectNotFoundException
+public void test002_hikeOpen() throws UiObjectNotFoundException
 
 {
 	
@@ -44,226 +46,245 @@ public void test002() throws UiObjectNotFoundException
 	
 }
 	
-//	public void testAutomation() throws UiObjectNotFoundException
-//
-//	{
-//
-//		try {
-//			getUiDevice().setOrientationNatural();
-//		} catch (RemoteException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//		//mdevice.setOrientationNatural();
-//
-//		// for accessing the 3 dot menu
-//		if(new UiObject(new UiSelector().className("android.widget.FrameLayout").index(0)).
-//				getChild(new UiSelector().className("android.widget.LinearLayout").index(0)).
-//				getChild(new UiSelector().className("android.widget.FrameLayout").index(0)).
-//				getChild(new UiSelector().className("android.view.View").index(0)).
-//				getChild(new UiSelector().className("android.widget.FrameLayout").index(0)). 
-//				getChild(new UiSelector().className("android.view.View").index(0)).
-//				getChild(new UiSelector().className("android.support.v7.widget.LinearLayoutCompat").index(1)).
-//				getChild(new UiSelector().className("android.widget.RelativeLayout")).exists())
-//		{
-//			new UiObject(new UiSelector().className("android.widget.FrameLayout").index(0)).
-//			getChild(new UiSelector().className("android.widget.LinearLayout").index(0)).
-//			getChild(new UiSelector().className("android.widget.FrameLayout").index(0)).
-//			getChild(new UiSelector().className("android.view.View").index(0)).
-//			getChild(new UiSelector().className("android.widget.FrameLayout").index(0)). 
-//			getChild(new UiSelector().className("android.view.View").index(0)).
-//			getChild(new UiSelector().className("android.support.v7.widget.LinearLayoutCompat").index(1)).
-//			getChild(new UiSelector().className("android.widget.RelativeLayout").index(3)).click();
-//		}
-//
-//
-//
-//		else
-//		{
-//			System.out.println("Wrong input");
-//		}
-//
-//	}
-//
-//
-//	//New group chat element click
-//	public void testElementClick() throws UiObjectNotFoundException
-//
-//	{
-//		UiObject rs = new UiObject(new UiSelector().className("android.widget.FrameLayout").index(0)).
-//				getChild(new UiSelector().className("android.widget.LinearLayout").index(0)).
-//				getChild(new UiSelector().className("android.widget.ListView").index(0)).
-//				getChild(new UiSelector().className("android.widget.LinearLayout").index(1));
-//
-//		if(rs.exists())
-//		{
-//			rs.click();
-//		}
-//		else
-//		{
-//			System.out.println("Incorect input");
-//		}
-//
-//	}
-//
-//	//Group chat creation screen
-//
-//	public void testNameEnter() throws UiObjectNotFoundException, InterruptedException
-//
-//	{
-//		System.out.println("setting group name");
-//
-//
-//
-//		new UiObject(new UiSelector().text("Name the group")).setText("Test");
-//
-//		/*UiDevice ut = UiDevice.getInstance();
-//	    ut.pressBack();*/
-//
-//
-//
-//		System.out.println("Umang, sleep 3000 before");
-//		Thread.sleep(3000);
-//		System.out.println("Umang, sleep 3000 after");
-//
-//
-//		UiObject next_1 = new UiObject(new UiSelector().className("android.widget.RelativeLayout").index(1));
-//		if(next_1.exists())
-//		{
-//			System.out.println("Umang, existing");
-//		}
-//		else 
-//		{
-//			System.out.println("Umang,not existing");
-//		}
-//		UiObject next_2 = next_1.getChild(new UiSelector().resourceId("com.bsb.hike:id/done_container").index(1));
-//
-//
-//
-//		if(next_2.exists())
-//		{
-//			System.out.println("click on next");
-//			next_2.click();	
-//		}
-//
-//		else
-//		{
-//			System.out.println("Incorect input");
-//		}
-//		System.out.println("Umang, sleep 4000 before");
-//		Thread.sleep(4000);	
-//		System.out.println("Umang, sleep 4000 before");
-//	}
-//
-//
-//
-//
-//	public void testPMemberAddition() throws UiObjectNotFoundException
-//	{
-//
-//
-//
-//		UiObject st = new UiObject(new UiSelector().className("android.widget.CheckBox"));
-//
-//
-//		if(!st.isChecked())
-//		{
-//
-//			st.click();
-//		}
-//
-//
-//		else
-//		{
-//			System.out.println("Wrong input");
-//		}
-//
-//		UiObject st4 = new UiObject(new UiSelector().className("android.widget.CheckBox").instance(1));
-//
-//		/*new UiObject(new UiSelector().className("android.widget.FrameLayout").index(0)).
-//				getChild(new UiSelector().className("android.widget.LinearLayout").index(0)).
-//				getChild(new UiSelector().className("android.widget.FrameLayout").index(0)).
-//				getChild(new UiSelector().className("android.view.View").index(0)).
-//				getChild(new UiSelector().className("android.widget.FrameLayout").index(1)).
-//				getChild(new UiSelector().className("android.widget.RelativeLayout").index(0)).
-//				getChild(new UiSelector().className("android.widget.LinearLayout").index(0)).
-//				getChild(new UiSelector().className("android.widget.ListView").index(2)).
-//				getChild(new UiSelector().className("android.widget.RelativeLayout").index(2)).
-//				getChild(new UiSelector().className("android.widget.RelativeLayout").index(2)).
-//				getChild(new UiSelector().className("android.widget.CheckBox").index(0)).click();*/
-//		st4.click();
-//
-//		System.out.println("Second user entered");
-//
-//
-//
-//
-//	}
-//
-//	public void testQDoneButtonClick() throws UiObjectNotFoundException
-//
-//	{
-//		System.out.println("Entered in queue");
-//
-//		try {
-//			Thread.sleep(4000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//
-//
-//		UiObject rs = new UiObject(new UiSelector().className("android.widget.RelativeLayout").index(0)).
-//				getChild(new UiSelector().resourceId("com.bsb.hike:id/done_container").index(2));
-//
-//
-//
-//
-//		if(rs.exists())
-//		{
-//			System.out.println("Button Click");
-//			rs.click();
-//		}
-//
-//
-//
-//		System.out.println("Success");
-//
-//	}
-//
-//
-//
-//
-//	public void testRTextEnter() throws UiObjectNotFoundException
-//
-//	{
-//		new UiObject(new UiSelector().className("android.widget.EditText").index(0)).setText("Hi");	
-//
-//		
-//	}	
-//
-//
-//	public void testSSendMsg() throws UiObjectNotFoundException
-//
-//	{
-//		System.out.println("About to click send button");
-//		try {
-//			Thread.sleep(4000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//		new UiObject(new UiSelector().className("android.widget.RelativeLayout").index(2)).
-//		getChild(new UiSelector().className("android.widget.RelativeLayout").index(0)).
-//		getChild(new UiSelector().resourceId("com.bsb.hike:id/send_message")).click();
-//
-//		System.out.println("clicked on send button");
-//
-//
-//	}
+	public void test003_OverflowMenuClick() throws UiObjectNotFoundException
+
+	{
+
+		try {
+			getUiDevice().setOrientationNatural();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+
+		// for accessing the 3 dot menu
+		if(new UiObject(new UiSelector().className("android.widget.FrameLayout").index(0)).
+				getChild(new UiSelector().className("android.widget.LinearLayout").index(0)).
+				getChild(new UiSelector().className("android.widget.FrameLayout").index(0)).
+				getChild(new UiSelector().className("android.view.View").index(0)).
+				getChild(new UiSelector().className("android.widget.FrameLayout").index(0)). 
+				getChild(new UiSelector().className("android.view.View").index(0)).
+				getChild(new UiSelector().className("android.support.v7.widget.LinearLayoutCompat").index(1)).
+				getChild(new UiSelector().className("android.widget.RelativeLayout")).exists())
+		{
+			new UiObject(new UiSelector().className("android.widget.FrameLayout").index(0)).
+			getChild(new UiSelector().className("android.widget.LinearLayout").index(0)).
+			getChild(new UiSelector().className("android.widget.FrameLayout").index(0)).
+			getChild(new UiSelector().className("android.view.View").index(0)).
+			getChild(new UiSelector().className("android.widget.FrameLayout").index(0)). 
+			getChild(new UiSelector().className("android.view.View").index(0)).
+			getChild(new UiSelector().className("android.support.v7.widget.LinearLayoutCompat").index(1)).
+			getChild(new UiSelector().className("android.widget.RelativeLayout").index(3)).click();
+		}
+
+
+
+		else
+		{
+			System.out.println("Wrong input");
+		}
+
+	}
+
+
+	//New group chat element click
+	public void test004_NewGroupOptionClick() throws UiObjectNotFoundException
+
+	{
+		UiObject rs = new UiObject(new UiSelector().className("android.widget.FrameLayout").index(0)).
+				getChild(new UiSelector().className("android.widget.LinearLayout").index(0)).
+				getChild(new UiSelector().className("android.widget.ListView").index(0)).
+				getChild(new UiSelector().className("android.widget.LinearLayout").index(1));
+
+		if(rs.exists())
+		{
+			rs.click();
+		}
+		else
+		{
+			System.out.println("Incorect input");
+		}
+
+	}
+
+	//Group chat creation screen
+
+	public void test005_GroupChatNameEnter() throws UiObjectNotFoundException, InterruptedException
+
+	{
+		System.out.println("setting group name");
+
+
+
+		new UiObject(new UiSelector().text("Name the group")).setText("Test");
+
+		/*UiDevice ut = UiDevice.getInstance();
+	    ut.pressBack();*/
+
+
+
+		System.out.println("Umang, sleep 3000 before");
+		Thread.sleep(3000);
+		System.out.println("Umang, sleep 3000 after");
+
+
+		UiObject next_1 = new UiObject(new UiSelector().className("android.widget.RelativeLayout").index(1));
+		if(next_1.exists())
+		{
+			System.out.println("Umang, existing");
+		}
+		else 
+		{
+			System.out.println("Umang,not existing");
+		}
+		UiObject next_2 = next_1.getChild(new UiSelector().resourceId("com.bsb.hike:id/done_container").index(1));
+
+
+
+		if(next_2.exists())
+		{
+			System.out.println("click on next");
+			next_2.click();	
+		}
+
+		else
+		{
+			System.out.println("Incorect input");
+		}
+		System.out.println("Umang, sleep 4000 before");
+		Thread.sleep(4000);	
+		System.out.println("Umang, sleep 4000 before");
+	}
+
+	public void test006_GroupChatMemberAddition() throws UiObjectNotFoundException
+	{
+
+
+
+		UiObject st = new UiObject(new UiSelector().className("android.widget.CheckBox"));
+
+
+		if(!st.isChecked())
+		{
+
+			st.click();
+		}
+
+
+		else
+		{
+			System.out.println("Wrong input");
+		}
+
+		UiObject st4 = new UiObject(new UiSelector().className("android.widget.CheckBox").instance(1));
+
+		
+		st4.click();
+
+		System.out.println("Second user entered");
+
+
+
+
+	}
+
+	public void test007_GroupChatDoneButtonClick() throws UiObjectNotFoundException
+
+	{
+		System.out.println("Entered in queue");
+
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+		UiObject rs = new UiObject(new UiSelector().className("android.widget.RelativeLayout").index(0)).
+				getChild(new UiSelector().resourceId("com.bsb.hike:id/done_container").index(2));
+
+
+
+
+		if(rs.exists())
+		{
+			System.out.println("Button Click");
+			rs.click();
+		}
+
+
+
+		System.out.println("Success");
+
+	}
+
+
+	public void test008_UiWatcherRegister() throws UiObjectNotFoundException
+	{
+	// Register watcher
+		
+	UiDevice.getInstance().registerWatcher("Hi", okCancelDialogWatcher);
+
+	// Run watcher
+	UiDevice.getInstance().runWatchers();
+	
+	}
+
+	public void test009_GroupChatTextEnter() throws UiObjectNotFoundException
+
+	{
+		new UiObject(new UiSelector().className("android.widget.EditText").index(0)).setText("Hi");	
+
+		
+	}	
+
+
+	public void test010_GroupChatSendMsg() throws UiObjectNotFoundException
+
+	{
+		System.out.println("About to click send button");
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		new UiObject(new UiSelector().className("android.widget.RelativeLayout").index(2)).
+		getChild(new UiSelector().className("android.widget.RelativeLayout").index(0)).
+		getChild(new UiSelector().resourceId("com.bsb.hike:id/send_message")).click();
+
+		System.out.println("clicked on send button");
+
+
+	}
+	
+	UiWatcher okCancelDialogWatcher = new UiWatcher() {
+		@Override
+		public boolean checkForCondition() {
+			UiObject okCancelDialog = new UiObject(new UiSelector().textStartsWith("Regional Keyboard"));
+			if(okCancelDialog.exists()){
+				Log.w("New Group", "Found the example OK/Cancel dialog");
+				UiObject okButton = new UiObject(new UiSelector().descriptionContains("image_close"));
+				try {
+					okButton.click();
+				} catch (UiObjectNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				return (okCancelDialog.waitUntilGone(25000));
+			}
+			return false;
+		}
+	};
+	
+	
+
 //
 //	//Attachment menu click
 //
